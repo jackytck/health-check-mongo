02b539f086b1e99c89064a19b290b5c9447a00f8
Jacky Tang <jackytck@gmail.com>
2018-04-24 22:40:08 +0800

    :hammer: simplify use case to check one connection only

--- src/index.js
+++ src/index.js
@@ -1,28 +1,19 @@
-import HealthCheckCore from './core'
-import Repository from './repository'
+const MongoClient = require('mongodb')
 
-export default class HealthCheck {
-  static do (configurations) {
-    return new Promise((resolve, reject) => {
-      const configuration = {
-        fnIsHealthGood: status => {
-          return (status.error === null)
-        },
-        fnName: status => {
-          return `${status.configuration.url}`
-        },
-        fnErrorMessage: status => {
-          return status.error.message
-        }
+function checkHealth ({ mongoURI }) {
+  return new Promise(resolve => {
+    MongoClient.connect(mongoURI, (error, db) => {
+      if (error) {
+        resolve({
+          error,
+          healthy: false
+        })
+      } else {
+        db.close()
+        resolve({ healthy: true })
       }
-
-      HealthCheckCore.do(configurations, Repository.test, configuration)
-        .then(result => {
-          resolve(result)
-        })
-        .catch(error => {
-          reject(error)
-        })
     })
-  }
+  })
 }
+
+module.exports = checkHealth
